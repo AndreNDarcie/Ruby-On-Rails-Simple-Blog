8b7ee942cf724469e1b15c91bb1cd3786ee790a1
André N. Darcie <andre_darcie@hotmail.com>
2016-07-07 00:43:20 -0300

    Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,14 @@
 class ArticlesController < ApplicationController
   # CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
+
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
   end
 
