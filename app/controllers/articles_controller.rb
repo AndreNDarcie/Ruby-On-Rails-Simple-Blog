4cbb42438adfd62d8ecd18cb148ceee21d07d48c
André N. Darcie <andre_darcie@hotmail.com>
2016-07-07 23:52:54 -0300

    Basic Authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   # CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy.
-
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
@@ -44,7 +45,7 @@ class ArticlesController < ApplicationController
 
     redirect_to articles_path
   end
-  
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
